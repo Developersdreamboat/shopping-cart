acafb10e864d9299b9241c2b0ad957a77fa92fbe
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2023-01-18 14:18:01 +0200

    Add interactive menu in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import MenuIcon from './svg/bars-solid.svg';
 import CloseIcon from './svg/xmark-solid.svg';
 import CartIcon from './svg/cart-shopping-solid.svg';
@@ -5,22 +6,26 @@ import {Link} from 'react-router-dom';
 import './css/Header.css';
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
+  const menuToggle = () => {
+    setToggle(!toggle);
+  };
   return (
     <header>
-      <div className="menu">
+      <div className="menu" onClick={menuToggle}>
         <img src={MenuIcon} alt="menu" width="25"></img>
       </div>
       <div className="logo">
         <h1><Link to="/">Shop</Link></h1>
       </div>
       <nav>
-        <ul>
+        <ul className={toggle ? "toggle" : ""}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/product">Product</Link></li>
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/login">Login / Sign up</Link></li>
-          <li className="close">
+          <li className="close" onClick={menuToggle}>
             <img src={CloseIcon} alt="close" width="25"></img>
           </li>
         </ul>
