82e6c1261b6c9f683535589bc2f2392cb839b802
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2023-01-18 23:27:11 +0200

    Add products layout

--- src/components/content/Products.js
+++ src/components/content/Products.js
@@ -1,7 +1,25 @@
+import { useContext } from 'react';
+import { DataContext } from '../DataContext';
+import '../css/Products.css';
+
 function Products() {
+
+  const { products } = useContext(DataContext);
+  
   return (
-    <div>
-        Products
+    <div id="product">
+        {
+          products.map(product => (
+            <div className="card" key={product._id}>
+              <img src={product.src} alt="product" />
+              <div className="content">
+                <h3>{product.title}</h3>
+                <span>${product.price}</span>
+                <button>Add to card</button>
+              </div>
+            </div>
+          ))
+        }
     </div>
   );
 }
